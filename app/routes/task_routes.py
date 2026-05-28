02d9541f925fd1efa721e5726fbc6c0282db9038
Gabriel <gabriel_reinaldo@outlook.com>
2026-05-28 11:55:33 -0300

    feat: add basic requesto for post

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,11 +1,26 @@
 from flask import Blueprint, jsonify, request
+from app.services import task_service
 
 task_bp = Blueprint('tasks', __name__, url_prefix='/api/v1/tasks')
 
 @task_bp.route('/', methods=["POST"])
-def create_tasks():
+def create_task():
     data = request.get_json()
-    return jsonify(data)
+
+    try:
+        task = task_service.create_task(data)
+
+        return jsonify({
+            "success": True,
+            "data": task
+        }), 201
+    
+    except ValueError as error:
+
+        return jsonify({
+            "sucess": False,
+            "message": str(error)
+        }), 400
 
 
 @task_bp.route('/', methods=["GET"])
