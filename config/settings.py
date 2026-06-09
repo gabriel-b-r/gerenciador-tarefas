af870918ec69c8164cd9e0244a9bb73e75ea6d25
Gabriel <gabriel_reinaldo@outlook.com>
2026-06-09 14:10:48 -0300

    feat(docker): add docker setup for project

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,6 @@ DATABASE_URI = (
     f"postgresql://"
     f"{os.getenv('DB_USER')}:"
     f"{os.getenv('DB_PASSWORD')}@"
-    f"{os.getenv('DB_HOST')}/"
+    f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/"
     f"{os.getenv('DB_NAME')}"
 )
