fba807b6b3852c8d6e1ea5804570527d67a6d97f
Gabriel <gabriel_reinaldo@outlook.com>
2026-06-19 10:57:55 -0300

    feat(tests): add test_create_task_returns_400() to tests

--- tests/routes/test_create_task.py
+++ tests/routes/test_create_task.py
@@ -1,18 +1,34 @@
 def test_create_task_returns_201(client):
-    #arrange
     payload = {
         "title": "test"
     }
 
-    #act
     response = client.post("/api/v1/tasks", json=payload)
 
     print(response.status_code)
     print(response.location)
 
-    #assert
     assert response.status_code == 201
 
     data = response.get_json()
 
+    assert data["success"] == True
     assert data["data"]["title"] == "test"
+    assert data["data"]["priority"] == "LOW"
+    assert data["data"]["status"] == "PENDING"
+
+
+def test_create_task_returns_400(client):
+    payload = {}
+
+    response = client.post("/api/v1/tasks", json=payload)
+
+    print(response.status_code)
+    print(response.location)
+
+    assert response.status_code == 400
+    
+    data = response.get_json()
+
+    assert data["sucess"] == False
+    assert data["message"] == "Title is required"
