9b993d405819a99b0d03cb34c50e1b8fb783002a
Gabriel Borges Reinaldo <65171922+gabriel-b-r@users.noreply.github.com>
2026-06-20 11:30:55 -0300

    Revert "Bugfix/endpoints"

--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from app.extensions.database import db
-from app.models.task import Task
-
-app = create_app()
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
